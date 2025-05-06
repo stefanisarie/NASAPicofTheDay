c4b779054d92c78db6919bb66091c0be2368705d
Stefan Isarie <stefan.isarie@gmail.com>
2025-05-06 18:26:05 +0300

    Vercel analytics

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
+
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
     <NuxtPage />
+    <Analytics />
   </UApp>
 </template>
